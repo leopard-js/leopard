eef2cd2d8b95b2c7de122d25b70074a396bbb6c2
Florrie <towerofnix@gmail.com>
2020-01-21 12:35:32 -0400

    Credit trumank for ADPCM decoder too!

--- scratch-js/lib/decode-adpcm-audio.mjs
+++ scratch-js/lib/decode-adpcm-audio.mjs
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2019 Nathan Dinsmore and Adroitwhiz
+ * Copyright (c) 2013-2019 Truman Kilen, Nathan Dinsmore, and Adroitwhiz
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
